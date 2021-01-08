7c3484e23033a94d7c8ccd52c0e15695b2bec54d
Elian <efelix.v@gmail.com>
2021-01-08 17:46:14 -0500

    Setup initial layout and control variables

--- app/src/main/java/com/example/androidsimpletodo/MainActivity.java
+++ app/src/main/java/com/example/androidsimpletodo/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.androidsimpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.solver.ArrayLinkedVariables;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> items;
+
+    Button btnAdd;
+    EditText etItem;
+    RecyclerView rvItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        etItem = findViewById(R.id.etItem);
+        rvItems = findViewById(R.id.rvItems);
+
+        etItem.setText("I'm doing this from Java!!");
+
+        items = new ArrayList<>();
+        items.add("Buy milk");
+        items.add("Go to the GYM");
+        items.add("Have fun!?");
+
     }
 }
